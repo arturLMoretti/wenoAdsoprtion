5528322c6312856a367234279247ac1889bac1d3
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-08-11 22:12:59 -0300

    Modify maid file

--- Principal_exemplo_WENO_n.m
+++ Principal_exemplo_WENO_n.m
@@ -14,9 +14,9 @@ Deltaz=1/N;
 z=0:Deltaz:1;
 
 % O problema se torna cada vez mais stiff quanto maior o Pe. Neste casos,
-% as diferenças finitas ainda dão conta, mas muitas oscilações são
-% obsrvadas. Quando Pe=1000, já é possível observar isso. Quanto maior Pe,
-% nem o WENO dá conta, mas acredito que daí as CCs devem ser modificadas
+% as diferenï¿½as finitas ainda dï¿½o conta, mas muitas oscilaï¿½ï¿½es sï¿½o
+% obsrvadas. Quando Pe=1000, jï¿½ ï¿½ possï¿½vel observar isso. Quanto maior Pe,
+% nem o WENO dï¿½ conta, mas acredito que daï¿½ as CCs devem ser modificadas
 
 Pe_1=100;
 PSI1_1=0;
@@ -28,7 +28,7 @@ NN=length(n);
 t=linspace(0,10,5000);
 C0=zeros(2*N,1);
 
-for i=1:1
+for i=1:NN
     LEGENDA(i,1)={strcat('n','{ }','=','{ }',num2str(n(i)))};
 end
 
@@ -40,5 +40,5 @@ xlabel 't^*'
 ylabel 'X_i'
 hold on
 end
-title 'Efeito do Parâmetro n nas curvas de ruptura'
+title 'Efeito do Parï¿½metro n nas curvas de ruptura'
 legend(LEGENDA)
